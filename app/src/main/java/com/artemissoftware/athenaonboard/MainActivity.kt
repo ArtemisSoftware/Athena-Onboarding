02f9380bbeb88a52e86c33bda1294d2a325c2218
gustavo <gustavo.isaac@gmail.com>
2021-10-23 18:28:52 +0100

    Layout view_dim

--- app/src/main/java/com/artemissoftware/athenaonboard/MainActivity.kt
+++ app/src/main/java/com/artemissoftware/athenaonboard/MainActivity.kt
@@ -6,34 +6,34 @@ import android.graphics.Point
 import android.graphics.drawable.BitmapDrawable
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.view.Gravity
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import android.widget.Button
 import android.widget.LinearLayout
 import android.widget.PopupWindow
 import androidx.annotation.MainThread
 import androidx.constraintlayout.widget.ConstraintLayout
 import android.util.DisplayMetrics
+import android.view.*
 import android.widget.ImageView
 
 
 class MainActivity : AppCompatActivity() {
 
 
+    lateinit var btn_show_popup_armory: View
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-
-        val btn_show_popup_help = findViewById<View>(R.id.btn_show_popup_help) as Button
-        val btn_show_popup_olympus = findViewById<View>(R.id.btn_show_popup_olympus) as Button
-
-
-        val img_show_popup_athena = findViewById<View>(R.id.img_show_popup_athena) as ImageView
-        val img_show_popup_logo = findViewById<View>(R.id.img_show_popup_logo) as ImageView
-        val img_show_popup_info = findViewById<View>(R.id.img_show_popup_info) as ImageView
+//
+//
+//        val btn_show_popup_help = findViewById<View>(R.id.btn_show_popup_help) as Button
+//        val btn_show_popup_olympus = findViewById<View>(R.id.btn_show_popup_olympus) as Button
+//
+//
+//        val img_show_popup_athena = findViewById<View>(R.id.img_show_popup_athena) as ImageView
+//        val img_show_popup_logo = findViewById<View>(R.id.img_show_popup_logo) as ImageView
+//        val img_show_popup_info = findViewById<View>(R.id.img_show_popup_info) as ImageView
 
 
         val displayMetrics = DisplayMetrics()
@@ -42,11 +42,15 @@ class MainActivity : AppCompatActivity() {
         val width = displayMetrics.widthPixels
 
 
-        //first quadrant
 
-        val btn_show_popup_armory = findViewById<View>(R.id.btn_show_popup_armory) as Button
-        val btn_show_popup_library = findViewById<View>(R.id.btn_show_popup_library) as Button
-        val img_show_popup_theme = findViewById<View>(R.id.img_show_popup_theme) as ImageView
+
+//        //first quadrant
+
+        btn_show_popup_armory = findViewById<View>(R.id.btn_show_popup_armory) as Button
+
+
+//        val btn_show_popup_library = findViewById<View>(R.id.btn_show_popup_library) as Button
+//        val img_show_popup_theme = findViewById<View>(R.id.img_show_popup_theme) as ImageView
 
 
         btn_show_popup_armory.setOnClickListener {
@@ -56,84 +60,103 @@ class MainActivity : AppCompatActivity() {
 
         }
 
-        btn_show_popup_library.setOnClickListener {
 
-            val popUpTutorial = PopUpTutorial()
-            popUpTutorial.showPopupWindow_v3(it, getPopUpPoint(R.id.btn_show_popup_library, displayMetrics))
-
-        }
-
-        img_show_popup_theme.setOnClickListener {
-
-            val popUpTutorial = PopUpTutorial()
-            popUpTutorial.showPopupWindow_v3(it, getPopUpPoint(R.id.img_show_popup_theme, displayMetrics))
-
-        }
-
-        //second quadrant
-
-
-        val btn_show_popup_tartarus = findViewById<View>(R.id.btn_show_popup_tartarus) as Button
-        val btn_show_popup_styx = findViewById<View>(R.id.btn_show_popup_styx) as Button
-
-
-        btn_show_popup_tartarus.setOnClickListener {
-
-            val popUpTutorial = PopUpTutorial()
-            popUpTutorial.showPopupWindow_v3(it, getPopUpPoint(R.id.btn_show_popup_tartarus, displayMetrics))
-
-        }
-
-        btn_show_popup_styx.setOnClickListener {
-
-            val popUpTutorial = PopUpTutorial()
-            popUpTutorial.showPopupWindow_v3(it, getPopUpPoint(R.id.btn_show_popup_styx, displayMetrics))
-
-        }
-
-        //third quadrant
-        //fourth quadrant
-
-        btn_show_popup_help.setOnClickListener {
-
-            val popUpTutorial = PopUpTutorial()
-            popUpTutorial.showPopupWindow_v3(it, getPopUpPoint(R.id.btn_show_popup_help, displayMetrics))
-
-        }
-
-        btn_show_popup_olympus.setOnClickListener {
-
-            val popUpTutorial = PopUpTutorial()
-            popUpTutorial.showPopupWindow_v3(it, getPopUpPoint(R.id.btn_show_popup_olympus, displayMetrics))
-
-        }
+//        lolo.waitForLayout {
+//
+//            val j = 0
+//
+//        }
 
 
 
 
-        img_show_popup_logo.setOnClickListener {
 
-            val popUpTutorial = PopUpTutorial()
-            popUpTutorial.showPopupWindow_v3(it, getPopUpPoint(R.id.img_show_popup_logo, displayMetrics))
 
-        }
 
-        img_show_popup_athena.setOnClickListener {
-
-            val popUpTutorial = PopUpTutorial()
-            popUpTutorial.showPopupWindow_v3(it, getPopUpPoint(R.id.img_show_popup_athena, displayMetrics))
-
-        }
-
-        img_show_popup_info.setOnClickListener {
-
-            val popUpTutorial = PopUpTutorial()
-            popUpTutorial.showPopupWindow_v3(it, getPopUpPoint(R.id.img_show_popup_info, displayMetrics))
-
-        }
+//        btn_show_popup_library.setOnClickListener {
+//
+//            val popUpTutorial = PopUpTutorial()
+//            popUpTutorial.showPopupWindow_v3(it, getPopUpPoint(R.id.btn_show_popup_library, displayMetrics))
+//
+//        }
+//
+//        img_show_popup_theme.setOnClickListener {
+//
+//            val popUpTutorial = PopUpTutorial()
+//            popUpTutorial.showPopupWindow_v3(it, getPopUpPoint(R.id.img_show_popup_theme, displayMetrics))
+//
+//        }
+//
+//        //second quadrant
+//
+//
+//        val btn_show_popup_tartarus = findViewById<View>(R.id.btn_show_popup_tartarus) as Button
+//        val btn_show_popup_styx = findViewById<View>(R.id.btn_show_popup_styx) as Button
+//
+//
+//        btn_show_popup_tartarus.setOnClickListener {
+//
+//            val popUpTutorial = PopUpTutorial()
+//            popUpTutorial.showPopupWindow_v3(it, getPopUpPoint(R.id.btn_show_popup_tartarus, displayMetrics))
+//
+//        }
+//
+//        btn_show_popup_styx.setOnClickListener {
+//
+//            val popUpTutorial = PopUpTutorial()
+//            popUpTutorial.showPopupWindow_v3(it, getPopUpPoint(R.id.btn_show_popup_styx, displayMetrics))
+//
+//        }
+//
+//        //third quadrant
+//        //fourth quadrant
+//
+//        btn_show_popup_help.setOnClickListener {
+//
+//            val popUpTutorial = PopUpTutorial()
+//            popUpTutorial.showPopupWindow_v3(it, getPopUpPoint(R.id.btn_show_popup_help, displayMetrics))
+//
+//        }
+//
+//        btn_show_popup_olympus.setOnClickListener {
+//
+//            val popUpTutorial = PopUpTutorial()
+//            popUpTutorial.showPopupWindow_v3(it, getPopUpPoint(R.id.btn_show_popup_olympus, displayMetrics))
+//
+//        }
+//
+//
+//
+//
+//        img_show_popup_logo.setOnClickListener {
+//
+//            val popUpTutorial = PopUpTutorial()
+//            popUpTutorial.showPopupWindow_v3(it, getPopUpPoint(R.id.img_show_popup_logo, displayMetrics))
+//
+//        }
+//
+//        img_show_popup_athena.setOnClickListener {
+//
+//            val popUpTutorial = PopUpTutorial()
+//            popUpTutorial.showPopupWindow_v3(it, getPopUpPoint(R.id.img_show_popup_athena, displayMetrics))
+//
+//        }
+//
+//        img_show_popup_info.setOnClickListener {
+//
+//            val popUpTutorial = PopUpTutorial()
+//            popUpTutorial.showPopupWindow_v3(it, getPopUpPoint(R.id.img_show_popup_info, displayMetrics))
+//
+//        }
+//
     }
 
 
+
+    override fun onWindowFocusChanged(hasFocus: Boolean) {
+
+        btn_show_popup_armory.performClick()
+    }
 
     private fun getCoordinates(viewId : Int): Point{
 
